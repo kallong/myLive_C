12a336bc46d405c43b4de27bf4f526958dcb09bd
kallong <40667293@qq.com>
2016-06-18 10:52:05 +0800

    Initial Commit

---
+++ mylive_C/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mylive_C
+//
+//  Created by 牟廷方 on 16/6/18.
+//  Copyright © 2016年 牟廷方. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
