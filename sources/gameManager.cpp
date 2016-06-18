d60e396ff26e482670aeb5ef8430c7493a5f6453
kallong <kevin_mou@163.com>
2016-06-18 13:37:46 +0800

    first ci

---
+++ sources/gameManager.cpp
@@ -0,0 +1,9 @@
+//
+//  gameManager.cpp
+//  mylive_C
+//
+//  Created by 牟廷方 on 16/6/18.
+//  Copyright © 2016年 牟廷方. All rights reserved.
+//
+
+#include "gameManager.hpp"
---
+++ sources/gameManager.h
@@ -0,0 +1,13 @@
+//
+//  gameManager.h
+//  mylive_C
+//
+//  Created by 牟廷方 on 16/6/18.
+//  Copyright © 2016年 牟廷方. All rights reserved.
+//
+
+#ifndef gameManager_h
+#define gameManager_h
+
+
+#endif /* gameManager_h */
